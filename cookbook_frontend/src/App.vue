90c4557094e9afc239bb8d71c9c2843ae552bbbd
Fabian Klöckner <mrsabifa@gmail.com>
2020-04-20 09:02:08 +0200

    Add frontend project

---
+++ cookbook_frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Quicksand", Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
