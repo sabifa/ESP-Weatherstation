f1623f7fdcc6f3314d3bc091f3dcf1bdbda62f08
Fabian Klöckner <mrsabifa@gmail.com>
2020-05-20 11:45:09 +0200

    Add code so that ef core is able to create the tables

--- weatherstation_backend/Data/DataContext.cs
+++ weatherstation_backend/Data/DataContext.cs
@@ -11,6 +11,17 @@ namespace Weatherstation.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserSensor>()
+                .HasKey(x => new { x.ApplicationUserId, x.SensorMacAddress });
+
+            base.OnModelCreating(builder);
+        }
+
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<SensorData> SensorData { get; set; }
+        public DbSet<UserSensor> UserSensors { get; set; }
     }
 }
